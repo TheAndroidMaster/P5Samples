b41a1a30cd5199967efdb50831a8429d5869257f
James Fenn <18jafenn90@gmail.com>
2017-10-01 10:48:17 -0400

    added spiralcircles

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Stuff I made messing around with [p5js](https://p5js.org/) in my free time.
 
 Projects:
 - [3D Rects](https://theandroidmaster.github.io/P5Samples/3drects/)
+- [Spiral Circles](https://theandroidmaster.github.io/P5Samples/spiralcircles/)
---
+++ spiralcircles/sketch.js
@@ -0,0 +1,50 @@
+var diff;
+var xOffset, yOffset;
+var aOffset;
+var cRed, cGreen, cBlue;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  background('black');
+  diff = 100;
+  xOffset = 0;
+  yOffset = 0;
+  aOffset = 0;
+  cRed = Math.random() * 200;
+  cGreen = Math.random() * 200;
+  cBlue = Math.random() * 200;
+}
+
+function draw() {
+  background(cRed, cGreen, cBlue);
+  noStroke();
+
+  xOffset = (mouseX - (window.innerWidth / 2.0)) / 10000;
+  yOffset = (mouseY - (window.innerHeight / 2.0)) / 10000;
+
+  var aRed = cRed;
+  var aGreen = cGreen;
+  var aBlue = cBlue;
+
+  for (var r = Math.max(window.innerWidth / 2, window.innerHeight / 2), a = 0; r > 0; r -= 3) {
+    var x = window.innerWidth / 2, y = window.innerHeight / 2;
+    x += Math.cos((a + (aOffset * a / 1000)) * Math.PI / 180) * r;
+    y += Math.sin((a + (aOffset * a / 1000)) * Math.PI / 180) * r;
+
+    fill(aRed, aGreen, aBlue);
+    ellipse(x + (xOffset * a), y + (yOffset * a), 100, 100);
+    a += 10;
+
+    aRed *= 1.005;
+    aGreen *= 1.005;
+    aBlue *= 1.005;
+  }
+
+  aOffset++;
+}
+
+function mouseClicked() {
+  cRed = Math.random() * 200;
+  cGreen = Math.random() * 200;
+  cBlue = Math.random() * 200;
+}
