02f59ef8af28a62025368afc7a2e47796807836f
TheAndroidMaster <18jafenn90@gmail.com>
2017-10-07 13:39:50 -0400

    added lines

---
+++ lines/sketch.js
@@ -0,0 +1,32 @@
+var diff;
+var xOffset, yOffset;
+var gRed, gGreen, gBlue;
+
+var objects;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+	gRed = random(200, 255);
+  gGreen = random(200, 255);
+  gBlue = random(200, 255);
+	background(gRed, gGreen, gBlue);
+
+	objects = [];
+	for (var i = 0; i < 50; i++) {
+		objects[i] = {};
+		objects[i].x = (Math.random() * (window.innerWidth - 100)) + 50;
+	}
+}
+
+function draw() {
+	background(gRed, gGreen, gBlue);
+  stroke(gRed - 40, gGreen - 40, gBlue - 40, 150);
+	strokeWeight(50);
+	fill(0, 0, 0, 0);
+	for (var i = 0; i < objects.length; i++) {
+		var object = objects[i];
+		var x = (Math.sin(frameCount / (10000 / i) + object.x) * (window.innerWidth - 100)) + 50;
+		x += mouseX / i;
+		line(x, 0, x + ((i % 2 == 0 ? 4 : -4) * window.innerWidth / objects.length), window.innerHeight);
+	}
+}
