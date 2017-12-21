cd8afafb0d0af0efc8cfbca0703014e186e94e36
James Fenn <18jafenn90@gmail.com>
2017-12-20 21:16:17 -0500

    I DO NOT LIKE SCALING THINGS

--- macbeth/sketch.js
+++ macbeth/sketch.js
@@ -3,31 +3,39 @@ var capture;
 var graphics;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  graphics = createGraphics(window.innerWidth, window.innerHeight, WEBGL);
+  createCanvas(windowWidth, windowHeight);
+  graphics = createGraphics(windowWidth, windowHeight, WEBGL);
   dagger = loadModel("dagger.obj", true);
-  capture = createCapture(VIDEO);
-  capture.size(window.innerWidth, window.innerHeight);
+  //capture = createCapture(VIDEO);
+  //capture.size(windowWidth, windowHeight);
 }
 
 function draw() {
   graphics.resetMatrix();
   graphics.background(0, 0, 0, 0);
   background(200);
-  image(capture, 0, 0, window.innerWidth, window.innerHeight);
+  //image(capture, 0, 0, windowWidth, windowHeight);
 
+  //graphics.translate(-windowWidth / 2, -windowHeight / 2);
   if (rotationX > 0 || rotationY > 0 || rotationZ > 0) {
     graphics.rotateX(-radians(rotationX));
     graphics.rotateY(-radians(rotationY));
     graphics.rotateZ(-radians(rotationZ));
   } else {
-    graphics.rotateX(Math.PI * -(mouseY / window.innerHeight));
-    graphics.rotateZ(Math.PI * (0.5 - (mouseX / window.innerWidth)));
+    graphics.rotateX(Math.PI * -(mouseY / windowHeight));
+    graphics.rotateZ(Math.PI * (0.5 - (mouseX / windowWidth)));
   }
 
-  graphics.pointLight(100, 100, 100, 0, window.innerHeight / 2, 0);
+  //graphics.translate(-windowWidth / 2, -windowHeight / 2, -0.5);
+
+  graphics.pointLight(100, 100, 100, 0, windowHeight / 2, 0);
   graphics.ambientMaterial(50);
   graphics.model(dagger);
 
   image(graphics, 0, 0);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  graphics.resizeCanvas(windowWidth, windowHeight);
 }
